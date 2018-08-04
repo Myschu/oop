978604a94a8cefc74010e07ccc23b5358068f5ed
Myschu <40799597+Myschu@users.noreply.github.com>
2018-08-04 18:25:41 -0400

    Add files via upload

--- token.cpp
+++ token.cpp
@@ -1,43 +1,73 @@
 #include "std_lib_facilities.h"
+#include "token.h"
 
-class Token {
-public:
-	char kind;
-	double value;
-};
 
-Token get_token();
-vector<Token> tokens;
-
-Token get_token() {
-	char ch;
-	cin >> ch;
-	switch (ch) {
-	case ';': case 'q': case '(': case ')': case'+': case'-': case'*': case'/': case'%':
-		return Token{ ch };
-	case'0': case'1': case'2': case'3': case'4': case'5': case'6': case'7': case'8': case'9': {
-		cin.putback(ch);
-		double val;
-		cin >> val;
-		return Token{ '8',val };
-	}
-	default:
-		return Token{ 'i',double(ch) };
-	}
+std::ostream &operator<<(std::ostream &os, Token const &t) {
+	return os << t.kind;
 }
 
 
-int main() {
-	for (Token t = get_token(); t.kind != 'q';t = get_token()) {
-		tokens.push_back(t);
+void Token_stream::putback(Token t) {
+	buffer.push_back(t);
+}
+
+Token Token_stream::popback() {
+	Token t = buffer.back();
+	buffer.pop_back();
+	return t;
+}
+
+Token Token_stream::get() {
+	if (buffer.size() > 0) {
+		Token t = popback();
+		return t;
+	}
+	char ch;
+	cin >> ch;
+	switch (ch) {
+	case print:
+	case quit:
+	case '(':
+	case ')':
+	case '[':
+	case ']':
+	case '+':
+	case '-':
+	case '/':
+	case '*':
+	case '=':
+	case mod:
+	case power:
+		return Token{ ch };
+	case '.':
+	case '0': case '1': case '2': case '3': case '4':
+	case '5': case '6': case '7': case '8': case '9':
+	{
+		cin.putback(ch);
+		double val;
+		cin >> val;
+		return Token{ number, val };
+	}
+	default:
+		if (isalpha(ch)) {
+			string s;
+			s += ch;
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+			cin.putback(ch);
+			return Token{ name, s };
+		}
+		return Token{ invalid, double(ch) };
+	}
+	return Token{ quit };
+}
+
+void Token_stream::ignore(char c) {
+	while (buffer.size() > 0) {
+		Token t = popback();
+		if (t.kind == c) return;
 	}
 
-	for (Token tok : tokens) {
-		if (tok.kind == '8')
-			cout << "A number token of val = " << tok.value << "\n";
-		else if (tok.kind == 'i')
-			cout << "We received an invalid token of value " << char(tok.value) << endl;
-		else
-			cout << "Token of kind " << tok.kind << "\n";
-	}
+	char ch = 0;
+	while (cin >> ch)
+		if (ch == c) return;
 }
